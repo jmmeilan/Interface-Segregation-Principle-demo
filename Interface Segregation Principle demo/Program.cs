d22efe96f48e95ba639a44bf0c8f08fd4e85d63f
Jose Miguel Meilan Maldonado <josemeilan242@gmail.com>
2021-06-13 15:18:29 +0200

    Add project files.

---
+++ Interface Segregation Principle demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Interface_Segregation_Principle_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
